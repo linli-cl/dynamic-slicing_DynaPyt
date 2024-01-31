93e9a54737514f3fd4562b6217fd73167ecaf11c
LiLin <root@LinLi>
2024-01-31 08:35:32 +0100

    change

--- test_milestone3.sh
+++ test_milestone3.sh
@@ -2,7 +2,7 @@
 
 TEST_DIR="tests/milestone3"
 
-for ((i=1; i<=10; i++)); do
+for ((i=1; i<=11; i++)); do
     TEST_PATH="$TEST_DIR/test_$i"
 
     INSTRUMENT_CMD="python -m dynapyt.instrument.instrument --analysis src.dynamicslicing.slice.Slice:\"$TEST_PATH/program.py\" --files \"$TEST_PATH/program.py\""
