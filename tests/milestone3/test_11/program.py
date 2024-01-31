666b8bc66d4cbedbe9329621dd7fdef5cee2088b
LiLin <root@LinLi>
2024-01-31 08:35:56 +0100

    add

---
+++ tests/milestone3/test_11/program.py
@@ -0,0 +1,23 @@
+# DYNAPYT: DO NOT INSTRUMENT
+
+
+import dynapyt.runtime as _rt
+
+_dynapyt_ast_ = "/home/liln/23w_program_analysis/myproject/Dynamic_Slicing_DynaPyt/tests/milestone3/test_11/program.py" + ".orig"
+try:
+    def slice_me():
+        ages = _rt._write_(_dynapyt_ast_, 1, [0, 25, 50, 75, 100, 150], [lambda: ages])
+        current_age = _rt._write_(_dynapyt_ast_, 4, _rt._sub_(_dynapyt_ast_, 3, _rt._read_(_dynapyt_ast_, 2, lambda: ages), [0]), [lambda: current_age])
+        while _rt._enter_while_(_dynapyt_ast_, 9, _rt._read_(_dynapyt_ast_, 5, lambda: current_age) < _rt._sub_(_dynapyt_ast_, 7, _rt._read_(_dynapyt_ast_, 6, lambda: ages), [-1])):
+            current_age += _rt._aug_assign_(_dynapyt_ast_, 8, lambda: current_age, 0, 1)
+        if _rt._enter_if_(_dynapyt_ast_, 24, _rt._read_(_dynapyt_ast_, 10, lambda: current_age) == _rt._sub_(_dynapyt_ast_, 12, _rt._read_(_dynapyt_ast_, 11, lambda: ages), [-2])):
+            ages[-1] += _rt._aug_assign_(_dynapyt_ast_, 15, lambda: _rt._sub_(_dynapyt_ast_, 14, _rt._read_(_dynapyt_ast_, 13, lambda: ages), [-1]), 0, 20)
+        elif _rt._enter_if_(_dynapyt_ast_, 23, _rt._read_(_dynapyt_ast_, 16, lambda: current_age) == _rt._sub_(_dynapyt_ast_, 18, _rt._read_(_dynapyt_ast_, 17, lambda: ages), [-1])):
+            ages[-1] += _rt._aug_assign_(_dynapyt_ast_, 21, lambda: _rt._sub_(_dynapyt_ast_, 20, _rt._read_(_dynapyt_ast_, 19, lambda: ages), [-1]), 0, 50)
+        else:
+            _rt._call_(_dynapyt_ast_, 22, print, False, [("", "something went wrong")], {})        
+        return _rt._read_(_dynapyt_ast_, 25, lambda: ages) # slicing criterion
+    
+    _rt._call_(_dynapyt_ast_, 27, _rt._read_(_dynapyt_ast_, 26, lambda: slice_me), False, [], {})
+except Exception as _dynapyt_exception_:
+    _rt._catch_(_dynapyt_exception_)
